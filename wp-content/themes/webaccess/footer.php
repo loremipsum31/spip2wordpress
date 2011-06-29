30fd884a0d6eb198a2c20110b932d6e330b2eb68
Rocio Alvarado <rocio.alvarado@gmail.com>
2011-06-29 14:04:33 -0400

    added proper code for links

--- wp-content/themes/webaccess/footer.php
+++ wp-content/themes/webaccess/footer.php
@@ -1,7 +1,16 @@
 <div id="footer">
 	<!-- The h1 element for the footer helps to maintain a proper order of the headers of the page. -->
 	<h1 class="screen_reader"><?php _e('Footer'); ?></h1>
-	<p>&copy; Coopérative AccessibilitéWeb, 2006-<?php echo date('Y'); ?>. Sous licence <a href="http://creativecommons.org/licenses/by-nd/3.0/deed.fr_CA" hreflang="fr_CA" lang="en_CA">Creative Commons BY-ND</a>. <a href="#"><?php _e('Accessibility'); ?></a> <a href="#"><?php _e('Site Map'); ?></a></p>
+	<?php 
+	/*
+	 * 
+	 * echo do_shortcode( '[contact-form 1 "Contact form 1"]' ); 
+	 * Google Maps à la mitaine.
+	 * Twitter ??
+	 * the_widget( 'ALO_Easymail_Widget' );
+	 */
+	 ?>
+	<p>&copy; Coopérative AccessibilitéWeb, 2006-<?php echo date('Y'); ?>. Sous licence <a href="http://creativecommons.org/licenses/by-nd/3.0/deed.fr_CA" hreflang="fr_CA" lang="en_CA">Creative Commons BY-ND</a>. <a href="/accessibilite"><?php _e('Accessibility', 'webaccess'); ?></a> <a href="/plan-du-site"><?php _e('Site Map', 'webaccess'); ?></a></p>
 </div>
 
 <?php wp_footer(); ?>
