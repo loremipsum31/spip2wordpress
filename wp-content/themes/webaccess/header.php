e00c6a5fa4c7587f6e6c2904c9434b375f8589c0
Rocio Alvarado <rocio.alvarado@gmail.com>
2011-06-29 14:59:36 -0400

    added title

--- wp-content/themes/webaccess/header.php
+++ wp-content/themes/webaccess/header.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
 <head>
-	<title><?php wp_title();?> <?php bloginfo('name');?></title>
+	<title>
+		<?php wp_title('-',true,'right'); ?>
+		<?php bloginfo('name'); ?>
+	</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>"/>
 	<!-- 
