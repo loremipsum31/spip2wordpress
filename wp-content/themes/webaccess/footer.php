9b85d0d586f52a896cae02327311db94fba6c65a
Rocio Alvarado <rocio.alvarado@gmail.com>
2011-07-25 16:33:49 -0400

    added more code in footer to test git functions

--- wp-content/themes/webaccess/footer.php
+++ wp-content/themes/webaccess/footer.php
@@ -1,6 +1,17 @@
 <div id="footer">
 	<!-- The h1 element for the footer helps to maintain a proper order of the headers of the page. -->
 	<h1 class="screen_reader"><?php _e('Footer'); ?></h1>
+	
+	<h2>Contactez-nous!</h2>
+	<p>
+	Coopérative AccessibilitéWeb<br />
+	1751, rue Richardson suite 6.111<br />
+	Montréal, (Québec), H3K 1G6<br />
+	Téléphone Sans-frais : 1-877-315-5550<br />
+	Télécopieur Sans-frais : 1-877-315-5550<br />
+	Région de Montréal : 514-312-3378<br />
+	Courriel: info@accessibiliteweb.com
+	</p>
 	<?php echo do_shortcode( '[contact-form 1 "Contact form 1"]' ); 
 	/*
 	 * 
